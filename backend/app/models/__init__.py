9b14ff272eef408262c23092a48d39f24dc7298a
Nick Rathbone <nrathbone@joinhandshake.com>
2025-10-01 13:06:06 -0600

    chore(models): expose Hero model in models package

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,4 @@
 # Marks models as a package and makes model imports cleaner
-from backend.app.models.models import Event, Entrant, Match, User
+from backend.app.models.models import Event, Entrant, Match, User, Hero
 
-__all__ = ["Event", "Entrant", "Match", "User"]
+__all__ = ["Event", "Entrant", "Match", "User", "Hero"]
